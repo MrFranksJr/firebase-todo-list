5a27b7ee286c8851d3f0ade38f334a21f7f5c09f
MrFranksJr <frankyjr@me.com>
2023-09-19 09:08:40 +0200

    latest updates

--- index.js
+++ index.js
@@ -11,10 +11,31 @@ const itemsInDB = ref(appDatabase, "shopping")
 
 const addButtonEl = document.getElementById('add-button')
 const inputFieldEl = document.getElementById('input-field')
+const shoppingListEl = document.getElementById('shopping-list')
 
 addButtonEl.addEventListener("click", addToList)
+inputFieldEl.addEventListener("keydown", inputFieldEventCheck)
 
 function addToList() {
     const inputValue = inputFieldEl.value
-    console.log(inputValue)
+
+    push(itemsInDB, inputValue)
+    console.log(`${inputValue} added to database`)
+
+    clearInputField(inputFieldEl)
+    addItemToScreen(inputValue)
+}
+
+function clearInputField(inputEl) {
+    inputEl.value = ""
+}
+
+function addItemToScreen(valueToAdd) {
+    shoppingListEl.innerHTML += `<li>${valueToAdd}</li>`
+}
+
+function inputFieldEventCheck(e) {
+    if (e.key === 'Enter') {
+        addToList()
+    }
 }
